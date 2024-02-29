1c1f11d5b9533de2237269cd2ae1a1b646e1e2c9
Katerina-Masterina <katerinka_94@mail.ru>
2024-02-29 23:56:29 +0900

    hw1

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,19 +1,13 @@
-setInterval(play(), 1000);
+setInterval(play, 1000);
 
 
-function play(index){
+function play(){
     const rotatorCase = document.querySelectorAll('.rotator__case');
-    const rotatorCaseActive = document.querySelectorAll('.rotator__case_active');
+    const rotatorCaseActive = document.querySelectorAll('.rotator__case.rotator__case_active');
 
 
-    for(index = 0; index < rotatorCase.length; index++){
-
-
-        if (rotatorCaseActive[index]){
-            rotatorCase[index].classList.remove('rotator__case_active');
-        } else {
-            rotatorCase[index].classList.add('rotator__case_active');
-        }
+    for(let i = 0; i < rotatorCase.length; i++){
+   rotatorCase[i].classList.toggle('rotator__case_active');
 
 }
 }
@@ -28,15 +22,3 @@ function play(index){
     // }
 
 
-    // getHole = index => document.getElementById(`hole${index}`),
-    // deactivateHole = index => getHole( index ).className = 'hole',
-    // activateHole = index => getHole( index ).className = 'hole hole_has-mole',
-
-
-// stars.forEach((star, index) => {
-//     star.addEventListener('click', () => {
-//         stars.forEach(s => s.classList.remove('star_active'));
-//         const activeStars = [...stars].slice(0, index + 1);
-//         activeStars.forEach((s) => s.classList.add('star_active'));
-//     })
-// })
