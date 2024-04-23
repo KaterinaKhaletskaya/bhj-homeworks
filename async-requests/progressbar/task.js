72a33a9154d5346012e612b6bc00141b61a02fca
Katerina-Masterina <katerinka_94@mail.ru>
2024-04-23 11:58:51 +0900

    hw2

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,51 +6,21 @@ button.addEventListener('click', (e) => {
 
 		let formData = new FormData(document.forms.form);
 
-
 		let xhr = new XMLHttpRequest();
-        if (xhr.readyState === 0) {
-            progress.value = 0.2;
-        }
+
+		xhr.upload.onprogress = function(event) {
+
+			if (event.loaded === event.total) {
+				progress.value = 1.0;
+			} else {
+                progress.value = event.loaded / 1000000;
+            }
+		}
 
 		xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
 
-
-
-		xhr.onloadstart = function() {
-		
-			if (xhr.readyState === 1) {
-				progress.value = 0.4;
-			}
-		}
-
 		xhr.send(formData);
 
-		if (xhr.readyState === 2) {
-            progress.value = 0.6;
-        }
-
-
-		xhr.onprogress = function() {
-
-            if (xhr.readyState === 3) {
-				progress.value = 0.8;
-			};
-		}
-
-		xhr.onload = function() {
- 
-			if (xhr.readyState === 4) {
-				progress.value = 1;
-                
-			}
-            
-		}
-
-       
 	}
-
-
-
-
 
 )
